aa516acc3e9d0d34afd10c30018fc34551bc3750
saffire fox <531eis@gmail.com>
2014-08-11 11:30:17 -0600

    Initial commit. Only does i/o with prompt and alerts.  Need to re-code to display results on document.

---
+++ rectangle_area/rectangle_area.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Ann Marie on 8/8/2014.
+ */
+
+function rectArea(){
+    var width = Number(prompt("Enter rectangle width:"));
+    var height = Number(prompt("Enter rectangle height:"));
+    var area = width * height;
+    alert("The rectangle area is: " + area);
+}
+
