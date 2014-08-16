18d3afd633bb7f9a017e436ebc24623905b3c10a
saffirefox <531eis@gmail.com>
2014-08-16 15:29:53 -0600

    initial commit. code not complete.

---
+++ how_old_are_you/js/age.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Ann Marie on 8/6/2014.
+ */
+
+
+//write  function to remove hyphens, dashes, spaces, or periods
+//var birthdate = "August";
+//var regexSPchar = /\W /ig;           //regex to find all special char incl. /, . , -
+
+
+
+
+
+
+
+
+
+/*if(birthdate.match(regexMaster) != '')
+{
+    removeChar = birthdate.replace(regexSlash,'');
+}
+else if(birthdate.match(regexDash) != ''){
+    removeChar = birthdate.replace(regexDash,'');
+}
+else if(birthdate.match(regexPeriod)){
+    removeChar = birthdate.match(regexPeriod,'');
+}
+else if(birthdate.match(regexSpace,'')){
+    removeChar = birthdate.replace(regexSpace,'');
+}
+
+
+*/
+
+
+
+
+
+/*birthdate = birthdate.split(regexSlash);
+console.log(birthdate);
+var newBirth = '';
+for(var i= 0; i < birthdate.length; i++){
+   newBirth += birthdate[i];
+}
+
+console.log(newBirth);
+*/
+/*
+var card = function(birthdate) {
+
+
+
+    //check if year diff is greater or less than 21
+    function diffYear(){
+
+
+
+    }
+    var today = new Date();
+    var year = today.getFullYear();
+    console.log(year);
+    console.log(today.getMonth() + 1);
+    var date
+    console.log(today.getDate());
+    console.log(today.getHours());
+    console.log(today.getMinutes());
+};
+
+card(age);
+
+    */
