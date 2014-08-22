57bd192fd7b39e3e86784f086266a62237b57254
saffirefox <531eis@gmail.com>
2014-08-22 12:21:09 -0600

    Initial commit. Code works.  Needs re-factoring.

---
+++ WeCard/js/WeCardYou.js
@@ -0,0 +1,150 @@
+/**
+ * Created by Ann Marie on 7/31/2014.
+ */
+//program to determine if person is of drinking age
+
+$(document).ready(function(){
+    $('#month').change(function() {
+        birthMonth();
+    });//end
+    $('#day').change(function(){
+        birthDay();
+    });//end
+    $('#year').change(function(){
+        birthYear();
+    });
+    $('#actionButton').click(function(){
+
+        calcAge();
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+function getTodayDate(){
+    //get today's year, month, day
+    var todayYear = new Date().getFullYear();
+    var todayMonth = new Date().getMonth() + 1;     //add 1 because January starts with index 0.
+    var todayDay = new Date().getDate();
+    console.log(todayYear);
+    console.log(todayMonth);
+    console.log(todayDay);
+
+
+}//end getTodayDate()
+
+//function to get month value from form
+function birthMonth(){
+
+    var bMonth = $('#month').val();
+    console.log(bMonth);
+    return bMonth;
+}
+
+//function to get day value from form
+function birthDay(){
+    var bDay = $('#day').val();
+    console.log(bDay);
+    return bDay;
+}
+
+//function to get year value from form
+function birthYear(){
+    var bYear = $('#year').val();
+    console.log(bYear);
+    return bYear;
+}
+
+function calcAge(){
+    //Initialize message as empty string. Will provide user with result of age calculation
+    var message = '';
+
+    //call functions and assign variables to their returned values
+   var mb = birthMonth();
+   var db = birthDay();
+   var yb = birthYear();
+
+   //create date object for today
+    var nowDate = new Date();
+    var nowMonth = nowDate.getMonth() + 1;      //add 1 since Jan starts at index 0
+    var nowDay = nowDate.getDate();
+    var nowYear = nowDate.getFullYear();
+    console.log(nowMonth + "" + nowDay + "" + nowYear);
+
+    //take difference between birth year and today's year
+    var yearDiff = nowYear - yb;
+    console.log(yearDiff);
+    //take difference between birth month and today's month. if difference is positive, it's ok but if negative, not ok.
+    var monthDiff = nowMonth - mb;
+    console.log(monthDiff);
+
+    var dayDiff = nowDay - db;
+    console.log(dayDiff);
+
+    //master if/else statements to determine if person is of drinking age
+    if(yearDiff > 21){
+        message = "Above age 21. Alcohol may be served.";
+        $('p').text(message);
+    }
+    else if(yearDiff < 21){
+        message = "Below age 21. Alcohol may NOT be served.";
+        $('p').text(message);
+    }
+    else{                   //if year difference is 21, needed check difference in months and days to determine if person is truly 21 or over
+        if(monthDiff < 0){
+            message = "Below age 21. Alcohol may NOT be served.";
+            $('p').text(message);
+        }else{
+            if(monthDiff ===0){                 //if birth month matches today's month, check the difference in days
+                if(db > nowDay) {
+                    message = "Below age 21. Alcohol may NOT be served.";
+                    $('p').text(message);
+                }
+                else{
+                        message = "Age 21 or over.";
+                        $('p').text(message);
+                    }
+                }//end day diff if months are equal
+            else if(monthDiff > 0){
+                        message = "Age 21 or over.";
+                        $('p').text(message);
+                }
+            else{
+                message = "Below age 21. Alcohol may NOT be served.";
+                $('p').text(message);
+                }
+            }//end monthDiff
+        }//end if/else yearDiff
+
+    }//end function calcAge()
+
+});//end document ready
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
